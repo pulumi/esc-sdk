465c7bd6e09d6338914401872fe222a0f173e501
Cleve Littlefield <cleverguy25@users.noreply.github.com>
2024-06-04 17:35:02 -0700

    chore: release 0.9.0 (#9)

--- sdk/python/pulumi_esc_sdk/configuration.py
+++ sdk/python/pulumi_esc_sdk/configuration.py
@@ -385,8 +385,7 @@ conf = pulumi_esc_sdk.Configuration(
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 0.1.0\n"\
-               "SDK Package Version: 0.1.1-dev.0".\
+               "SDK Package Version: 0.9.0-dev.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):

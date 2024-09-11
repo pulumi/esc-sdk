0764d5e4e645aaaf8afef1b8584efb6fcdef56c9
Cleve Littlefield <cleverguy25@users.noreply.github.com>
2024-09-11 08:44:34 -0700

    [chore]: release 0.10.0 (#22)

--- sdk/typescript/esc/raw/api.ts
+++ sdk/typescript/esc/raw/api.ts
@@ -15,7 +15,7 @@
  */
 
 
-const userAgent = 'esc-sdk/ts/0.9.2-dev.0';
+const userAgent = 'esc-sdk/ts/0.10.0-dev.0';
 
 import type { Configuration } from './configuration';
 import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';

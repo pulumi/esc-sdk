7c83f8f1f1dddc5b80c3af895a9acc939587b507
Sean Yeh <109418+seanyeh@users.noreply.github.com>
2024-10-28 18:21:53 -0500

    [chore]: release 0.10.3 (#49)

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "pulumi-esc-sdk"
-VERSION = "0.10.2.dev.0"
+VERSION = "0.10.3.dev.0"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",

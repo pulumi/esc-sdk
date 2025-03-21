405b92ecba2a67dd11e083d6f3ece8b380fe8a85
IaroslavTitov <iaro@pulumi.com>
2025-03-21 11:21:59 -0600

    Codefreeze for 0.12.0

--- sdk/typescript/esc/raw/api.ts
+++ sdk/typescript/esc/raw/api.ts
@@ -15,7 +15,7 @@
  */
 
 
-const userAgent = 'esc-sdk/ts/0.11.0-dev.0';
+const userAgent = 'esc-sdk/ts/0.12.0-dev.0';
 
 import type { Configuration } from './configuration';
 import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';

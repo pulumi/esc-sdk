b636f3cf74ce1468a5357f9fb5ed1b0318c62089
Sean Yeh <109418+seanyeh@users.noreply.github.com>
2025-10-10 17:07:10 -0500

    Release 0.12.3

--- sdk/python/pulumi_esc_sdk/configuration.py
+++ sdk/python/pulumi_esc_sdk/configuration.py
@@ -376,7 +376,7 @@ conf = pulumi_esc_sdk.Configuration(
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "SDK Package Version: 0.12.2.dev.0".\
+               "SDK Package Version: 0.12.3.dev.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
